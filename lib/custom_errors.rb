8029e02f68c6671d6728534715d29bbe166df896
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-01-03 02:13:49 +0000

    Done.

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,19 +1,3 @@
-def get_married(person)
-    self.partner = person
-    if person.class != Person
-      begin
-        raise PartnerError
-      rescue PartnerError => error
-          puts error.message
-      end
-    else
-      person.partner = self
-    end
-end
-If the object passed into the method as an argument is not an instance of the Person class, we will begin our error handling. First, we raise our PartnerError, then we rescue our PartnerError. The rescue method creates an instance of the PartnerError class and puts out the result of calling message on that instance.
-
-At this point, the code in custom_errors.rb should look like this:
-
 class Person
   attr_accessor :partner, :name
  
@@ -40,7 +24,7 @@ class Person
     end
   end
 end
-
-def pass 
-puts "pass"
-end
+ 
+beyonce = Person.new("Beyonce")
+beyonce.get_married("Jay-Z")
+puts beyonce.name
